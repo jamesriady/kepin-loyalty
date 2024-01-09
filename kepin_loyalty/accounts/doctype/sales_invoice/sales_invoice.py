89f1b25687348870d3f3e099e50946a13b7bb2d1
James Riady <james.riady@spenmo.com>
2024-01-09 14:27:20 +0000

    feat: reset earned loyalty points when the setting is off

--- kepin_loyalty/accounts/doctype/sales_invoice/sales_invoice.py
+++ kepin_loyalty/accounts/doctype/sales_invoice/sales_invoice.py
@@ -6,20 +6,25 @@ from erpnext.accounts.doctype.loyalty_program.loyalty_program import \
 
 def on_claim_loyalty_point(doc, method=None):
     if not doc.is_return and doc.loyalty_program:
+        lp_details = get_loyalty_program_details_with_points(doc.customer, company=doc.company,
+				loyalty_program=doc.loyalty_program, include_expired_entry=True)
+        # reset loyalty points only if the get point on redeem setting is False and the user redeems the point 
+        if not lp_details.get_point_on_redeem and doc.redeem_loyalty_points and doc.loyalty_points:
+            reset_earned_loyalty_points(doc)
+
         remaining_points = get_remaining_points(doc)
         set_previous_point(doc, remaining_points)
     elif doc.is_return and doc.return_against and doc.loyalty_program:
         against_si_doc = frappe.get_doc("Sales Invoice", doc.return_against)
-        
         against_si_doc.delete_loyalty_point_entry()
+        make_loyalty_point_entries_on_return(doc, against_si_doc)
 
-        revert_deleted_invoice_loyalty_point_entry(against_si_doc)
-        remaining_points = get_remaining_points(against_si_doc, filters={'creation': ['<', against_si_doc.creation]})
-        set_previous_point(against_si_doc, remaining_points)
+        against_si_remaining_points = get_remaining_points(against_si_doc, filters={'creation': ['<', against_si_doc.creation]})
+        set_previous_point(against_si_doc, against_si_remaining_points)
 
-        make_return_invoice_loyalty_point_entry(doc, against_si_doc)
-        remaining_points = get_remaining_points(doc)
-        set_previous_point(doc, remaining_points)
+        doc_remaining_points = get_remaining_points(doc)
+        set_previous_point(doc, doc_remaining_points)
+
 
 def on_cancel_claim_loyalty_point(doc, method=None):
     if doc.is_return and doc.return_against and doc.loyalty_program:
@@ -28,52 +33,53 @@ def on_cancel_claim_loyalty_point(doc, method=None):
         return_si_doc.delete_loyalty_point_entry()
 
         against_si_doc = frappe.get_doc("Sales Invoice", doc.return_against)
+        lp_details = get_loyalty_program_details_with_points(against_si_doc.customer, company=against_si_doc.company,
+				loyalty_program=against_si_doc.loyalty_program, include_expired_entry=True)
+        # reset loyalty points only if the get point on redeem setting is False and the user redeems the point 
+        if not lp_details.get_point_on_redeem and against_si_doc.redeem_loyalty_points and against_si_doc.loyalty_points:
+            reset_earned_loyalty_points(against_si_doc)
+
         remaining_points = get_remaining_points(against_si_doc, filters={'creation': ['<', against_si_doc.creation]})
         set_previous_point(against_si_doc, remaining_points)
 
-def make_return_invoice_loyalty_point_entry(doc, against_si_doc):
-    returned_amount = against_si_doc.get_returned_amount()
-    current_amount = flt(against_si_doc.grand_total) - cint(against_si_doc.loyalty_amount)
-    eligible_amount = returned_amount
-
-    lp_details = get_loyalty_program_details_with_points(against_si_doc.customer, company=against_si_doc.company,
-        current_transaction_amount=current_amount, loyalty_program=against_si_doc.loyalty_program,
-        expiry_date=against_si_doc.posting_date, include_expired_entry=True)
-    if lp_details and getdate(lp_details.from_date) <= getdate(against_si_doc.posting_date) and \
-        (not lp_details.to_date or getdate(lp_details.to_date) >= getdate(against_si_doc.posting_date)):
-
-        collection_factor = lp_details.collection_factor if lp_details.collection_factor else 1.0
-        points_earned = cint(eligible_amount/collection_factor)
-
-        new_doc = frappe.get_doc({
-            "doctype": "Loyalty Point Entry",
-            "company": doc.company,
-            "loyalty_program": lp_details.loyalty_program,
-            "loyalty_program_tier": lp_details.tier_name,
-            "customer": doc.customer,
-            "sales_invoice": doc.name,
-            "loyalty_points": -1 * points_earned,
-            "purchase_amount": eligible_amount,
-            "expiry_date": add_days(doc.posting_date, lp_details.expiry_duration),
-            "posting_date": doc.posting_date
-        })
-        new_doc.flags.ignore_permissions = 1
-        new_doc.save()
-        doc.set_loyalty_program_tier()
-
-def revert_deleted_invoice_loyalty_point_entry(doc):
-    returned_amount = 0
-    current_amount = flt(doc.grand_total) - cint(doc.loyalty_amount)
+def make_loyalty_point_entries_on_return(doc, against_doc):
+    returned_amount = against_doc.get_returned_amount()
+    current_amount = flt(against_doc.grand_total) - cint(against_doc.loyalty_amount)
     eligible_amount = current_amount - returned_amount
-    lp_details = get_loyalty_program_details_with_points(doc.customer, company=doc.company,
-        current_transaction_amount=current_amount, loyalty_program=doc.loyalty_program,
-        expiry_date=doc.posting_date, include_expired_entry=True)
-    if lp_details and getdate(lp_details.from_date) <= getdate(doc.posting_date) and \
-        (not lp_details.to_date or getdate(lp_details.to_date) >= getdate(doc.posting_date)):
+    lp_details = get_loyalty_program_details_with_points(against_doc.customer, company=against_doc.company,
+        current_transaction_amount=current_amount, loyalty_program=against_doc.loyalty_program,
+        expiry_date=against_doc.posting_date, include_expired_entry=True)
+    if lp_details and getdate(lp_details.from_date) <= getdate(against_doc.posting_date) and \
+        (not lp_details.to_date or getdate(lp_details.to_date) >= getdate(against_doc.posting_date)):
 
         collection_factor = lp_details.collection_factor if lp_details.collection_factor else 1.0
-        points_earned = cint(eligible_amount/collection_factor)
+        points_before_return_earned = cint(current_amount/collection_factor)
+        points_after_return_earned = cint(eligible_amount/collection_factor)
 
+        return_points_earned = points_after_return_earned - points_before_return_earned, # loyalty_points should be negative
+
+        if not lp_details.get_point_on_redeem:
+            if against_doc.redeem_loyalty_points and against_doc.loyalty_points:
+                points_before_return_earned = 0
+                return_points_earned = 0
+
+        # loyalty point entry for sales invoice
+        new_against_doc = frappe.get_doc({
+            "doctype": "Loyalty Point Entry",
+            "company": against_doc.company,
+            "loyalty_program": lp_details.loyalty_program,
+            "loyalty_program_tier": lp_details.tier_name,
+            "customer": against_doc.customer,
+            "sales_invoice": against_doc.name,
+            "loyalty_points": points_before_return_earned,
+            "purchase_amount": current_amount,
+            "expiry_date": add_days(against_doc.posting_date, lp_details.expiry_duration),
+            "posting_date": against_doc.posting_date
+        })
+        new_against_doc.flags.ignore_permissions = 1
+        new_against_doc.save()
+
+        # loyalty point entry for sales invoice return
         new_doc = frappe.get_doc({
             "doctype": "Loyalty Point Entry",
             "company": doc.company,
@@ -81,13 +87,14 @@ def revert_deleted_invoice_loyalty_point_entry(doc):
             "loyalty_program_tier": lp_details.tier_name,
             "customer": doc.customer,
             "sales_invoice": doc.name,
-            "loyalty_points": points_earned,
-            "purchase_amount": eligible_amount,
+            "loyalty_points": return_points_earned,
+            "purchase_amount": -1 * returned_amount,
             "expiry_date": add_days(doc.posting_date, lp_details.expiry_duration),
             "posting_date": doc.posting_date
         })
         new_doc.flags.ignore_permissions = 1
         new_doc.save()
+
         doc.set_loyalty_program_tier()
 
 def get_remaining_points(doc, filters={}):
@@ -108,26 +115,36 @@ def get_remaining_points(doc, filters={}):
     return frappe.db.get_value('Loyalty Point Entry', filters, 'sum(loyalty_points)')
 
 def set_previous_point(doc, remaining_points):
-    expiry_date = doc.posting_date
-    if not expiry_date:
-        expiry_date = today()
-
     if not remaining_points:
         remaining_points = 0
         
-    new_lp_entries = frappe.db.get_list('Loyalty Point Entry', 
+    lp_entries = frappe.db.get_list('Loyalty Point Entry', 
         filters={
             'customer': doc.customer, 
             'sales_invoice': doc.name, 
-            'loyalty_program': doc.loyalty_program, 
-            'expiry_date': ['>=', expiry_date],
+            'loyalty_program': doc.loyalty_program,
             'company': doc.company
         }, 
         fields=['name', 'loyalty_points'],
         order_by='creation'
     )
     
-    for lp_entry in new_lp_entries:
+    for lp_entry in lp_entries:
         doc = frappe.get_doc('Loyalty Point Entry', lp_entry.get('name'))
         doc.db_set('previous_loyalty_points', remaining_points)
         remaining_points = remaining_points + lp_entry.get('loyalty_points')
+
+def reset_earned_loyalty_points(doc):
+    lp_entries = frappe.db.get_list('Loyalty Point Entry', 
+        filters={
+            'customer': doc.customer, 
+            'sales_invoice': doc.name, 
+            'loyalty_program': doc.loyalty_program,
+            'company': doc.company,
+            'loyalty_points': ['>', 0]
+        }, 
+        fields=['name', 'loyalty_points'],
+        order_by='creation'
+    )
+    for lp_entry in lp_entries:
+        frappe.db.set_value('Loyalty Point Entry', lp_entry.get('name'), 'loyalty_points', 0, update_modified=False)
